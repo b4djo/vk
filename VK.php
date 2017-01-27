1f9e64b900462f7231c90a90ea0cc63d80cdbdd2
Robert <robertooo1990@gmail.com>
2017-01-28 02:36:40 +0300

    Add class vk

---
+++ VK.php
@@ -0,0 +1,35 @@
+<?php
+class VK {
+
+    private $access_token;
+    private $url = "https://api.vk.com/method/";
+
+    /**
+     * Конструктор
+     */
+    public function __construct($access_token) {
+
+        $this->access_token = $access_token;
+    }
+
+    /**
+     * Делает запрос к Api VK
+     * @param $method
+     * @param $params
+     */
+    public function method($method, $params = null) {
+
+        $p = "";
+        if( $params && is_array($params) ) {
+            foreach($params as $key => $param) {
+                $p .= ($p == "" ? "" : "&") . $key . "=" . urlencode($param);
+            }
+        }
+        $response = file_get_contents($this->url . $method . "?" . ($p ? $p . "&" : "") . "access_token=" . $this->access_token);
+
+        if( $response ) {
+            return json_decode($response);
+        }
+        return false;
+    }
+}
