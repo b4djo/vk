10ab53b8ef7225d0efa429d8140267d543ee3736
Robert <robertooo1990@gmail.com>
2017-01-28 02:40:24 +0300

    <actions> Add action wall post

---
+++ vk_post.php
@@ -0,0 +1,15 @@
+<?php
+
+include "vk.php";
+$access_token = "";
+$user_id = "";
+
+//Инициализируем класс
+$vk = new Vk($access_token);
+
+$params = array(
+    "owner_id" => $user_id,
+    "message" => "Hello world!",
+	'from_group' => 1
+);
+$post = $vk->method("wall.post", $params);
