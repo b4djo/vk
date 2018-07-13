b3078284f8666241b795593b888e70544d1b5ec4
Robert <robert@eljur.ru>
2018-07-13 17:32:25 +0300

    <master> Added default api version

--- app/lib/VK.php
+++ app/lib/VK.php
@@ -8,6 +8,9 @@ namespace b4djo\vk\app\lib;
  */
 class VK
 {
+    const V_5_70 = '5.70';
+    const V_5_80 = '5.80';
+
     /**
      * @var string
      */
@@ -19,22 +22,33 @@ class VK
     private $url = "https://api.vk.com/method/";
 
     /**
+     * @var string
+     */
+    private $v = self::V_5_80;
+
+    /**
      * VK constructor
      *
-     * @param $access_token
+     * @param string $access_token
+     * @param string|null $v
      */
-    public function __construct($access_token)
+    public function __construct(string $access_token, string $v = null)
     {
         $this->access_token = $access_token;
+
+        if ($v) {
+            $this->validVersion($v);
+            $this->v = $v;
+        }
     }
 
     /**
-     * @param $method
-     * @param null $params
+     * @param string $method
+     * @param array|null $params
      *
      * @return bool|mixed
      */
-    public function method($method, $params = null)
+    public function method(string $method, $params = null)
     {
         $p = "";
         if ($params && is_array($params)) {
@@ -42,12 +56,32 @@ class VK
                 $p .= ($p == "" ? "" : "&") . $key . "=" . urlencode($param);
             }
         }
-        $response = file_get_contents($this->url . $method . "?" . ($p ? $p . "&" : "") . "access_token=" . $this->access_token);
+        $response = file_get_contents($this->url . $method . "?" . ($p ? $p . "&" : "") . "access_token=" . $this->access_token . '&v' . $this->v);
 
         if ($response) {
             return json_decode($response);
         }
 
         return false;
+    }
+
+    /**
+     * @param string $version
+     */
+    private function validVersion($version)
+    {
+        $validVersions = [
+            static::V_5_70,
+            static::V_5_80,
+        ];
+
+        if (!in_array($version, $validVersions)) {
+            throw new \InvalidArgumentException(sprintf(
+                    'Unknown version %s, Supported versions: %s.',
+                    $version,
+                    implode(', ', $validVersions)
+                )
+            );
+        }
     }
 }
